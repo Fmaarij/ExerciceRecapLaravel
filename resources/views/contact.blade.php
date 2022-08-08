7e93bcf686854fdacfccff0ba0acbdc609792a74
Faisal Maarij <maarij_faisal@hotmail.com>
2022-08-08 15:15:41 +0200

    exercice recap à améliorer

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+
+<h1>C'est la page de contact</h1>
+
+@endsection
---
+++ resources/views/info.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('content')
+
+<h1>C'est la page de info</h1>
+
+@endsection
